0bed33dac5b5ddbc58793361cd59ba51d03f74dd
AlumulaRohithReddy <160841597+AlumulaRohithReddy@users.noreply.github.com>
2026-02-09 21:00:59 +0530

    Day 29 .Net Assignment

---
+++ Day 29 .Net Assignment/Delegatesdemo/Delegatesdemo/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Delegatesdemo
+{
+    internal class Program
+    {
+        delegate void Print();
+        class Money
+        {
+            protected int note;
+            protected int coin;
+            public Money(int n, int c)
+            {
+                this.note = n;
+                this.coin = c;
+            }
+        }
+        class Rupee : Money
+        {
+            public Rupee(int rupees, int paise) : base(rupees, paise) { }
+            public void Display()
+            {
+                Console.WriteLine("Rs.{0}.{1}", note, coin);
+            }
+        }
+        class Dollar : Money
+        {
+            public Dollar(int dollar, int cent) : base(dollar, cent) { }
+            public void Info()
+            {
+                Console.WriteLine("${0}.{1}", note, coin);
+            }
+        }
+        class Pound : Money
+        {
+            public Pound(int pound, int cent) : base(pound, cent) { }
+            public void Info()
+            {
+                Console.WriteLine("£{0}.{1}", note, coin);
+            }
+        }
+        class Test
+        {
+            static void write(Print[] p)
+            {
+                p[0]();
+                p[1]();
+                p[2]();
+            }
+            static void Main(string []Args)
+            {
+                Rupee m1 = new Rupee(1000, 55); 
+                Dollar m2 = new Dollar(100, 75); 
+                Pound m3=new Pound(123, 100);
+                Print[] p = new Print[3];
+                p[0] = new Print(m1.Display); 
+                p[1] = new Print(m2.Info);
+                p[2] = new Print(m3.Info);
+                write(p);
+            }
+            
+        }
+    }
+}
---
+++ Day 29 .Net Assignment/Linqdemo/Linqdemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Linqdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var filteredResult = integerList.Where(item => item%2==0);
+            foreach (var item in filteredResult)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            var sortedDescendingResult = integerList.OrderByDescending(item =>item);
+            foreach (var item in sortedDescendingResult)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            var groupedResult = integerList.GroupBy(n => n % 2 == 0 ? "Even" : "Odd");
+            foreach (var group in groupedResult)
+            {
+                Console.WriteLine(group.Key);
+                foreach (var num in group)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            var QuerySyntax = from obj in integerList
+                              where obj > 5
+                              select obj;
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+            integerList[2] = 10;
+            Console.WriteLine();
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+            //Console.ReadKey();
+        }
+    }
+}
+
