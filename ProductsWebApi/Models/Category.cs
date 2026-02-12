679a0d2d52a1e63feccb4c54a33933df568d8635
AlumulaRohithReddy <160841597+AlumulaRohithReddy@users.noreply.github.com>
2026-02-12 23:56:02 +0530

    Day 32 .Net Assignment

---
+++ ProductsWebApi/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using ProductsWebApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductsWebApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        // Navigation property (One-to-Many relationship)
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ ProductsWebApi/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProductsWebApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
