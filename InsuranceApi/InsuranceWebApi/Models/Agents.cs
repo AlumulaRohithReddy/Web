9455a5a8c08c14f11072a1b43077099abc34bd23
AlumulaRohithReddy <160841597+AlumulaRohithReddy@users.noreply.github.com>
2026-02-16 23:38:40 +0530

    Week7 .Net Assignment

---
+++ InsuranceApi/InsuranceWebApi/Models/Agents.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceWebApi.Models
+{
+    public class Agents
+    {
+        [Key]
+        public int AgentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
